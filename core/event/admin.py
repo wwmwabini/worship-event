1c0e71fb868ed7db5ea3d2295fc66063894ccd3e
Wallace Mwabini <wwmwabini@gmail.com>
2026-03-07 18:34:10 +0300

    Updated admin.py to display registrations

--- core/event/admin.py
+++ core/event/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Registration
 
-# Register your models here.
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'created_at')
+
+admin.site.register(Registration, RegistrationAdmin)
